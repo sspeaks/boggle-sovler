541fa052a72a9d82dee10348d6809871ffed6859
Seth Speaks <sspeaks610@gmail.com>
2025-03-12 17:00:21 -0700

    add log message to indicate server is running

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,42 @@
-{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import           Control.Monad.IO.Class   (MonadIO (liftIO))
-import           Data.Aeson               (ToJSON, encode)
-import qualified Data.Array               as A
-import           Data.Char                (toLower)
-import           Data.List                (nub, sortBy)
-import           Data.Map.Strict          (Map)
-import qualified Data.Map.Strict          as Map
-import           Data.Ord                 (Down (..), comparing)
-import qualified Data.Set                 as S
-import qualified Data.Text                as T
-import qualified Data.Text.IO             as TIO
-import           GHC.Generics             (Generic)
-import           Network.HTTP.Types       (hContentType, status200, status400,
-                                           status500)
-import           Network.Wai              (Application, pathInfo, responseLBS)
-import           Network.Wai.Handler.Warp (run)
-import           System.Environment       (getExecutablePath)
-import           System.FilePath          (takeDirectory, (</>))
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.Aeson (ToJSON, encode)
+import Data.Array qualified as A
+import Data.Char (toLower)
+import Data.List (nub, sortBy)
+import Data.Map.Strict (Map)
+import Data.Map.Strict qualified as Map
+import Data.Ord (Down (..), comparing)
+import Data.Set qualified as S
+import Data.Text qualified as T
+import Data.Text.IO qualified as TIO
+import GHC.Generics (Generic)
+import Network.HTTP.Types
+  ( hContentType,
+    status200,
+    status400,
+    status500,
+  )
+import Network.Wai (Application, pathInfo, responseLBS)
+import Network.Wai.Handler.Warp (run)
+import System.Environment (getExecutablePath)
+import System.FilePath (takeDirectory, (</>))
 
 -- Define the Trie data structure
 data Trie = Trie
   { endOfWord :: Bool,
-    children  :: Map Char Trie
+    children :: Map Char Trie
   }
   deriving (Show)
-
--- exampleBoard :: [Char]
--- exampleBoard = map toLower "IROOMADLEONFLWET"
 
 type GameBoard = A.Array (Int, Int) Char
 
 gameBoard :: String -> GameBoard
-gameBoard exampleBoard = A.array ((0, 0), (squareRoot-1, squareRoot-1)) $ zip [(i, j) | i <- [0 .. squareRoot-1], j <- [0 .. squareRoot-1]] (map toLower exampleBoard)
+gameBoard exampleBoard = A.array ((0, 0), (squareRoot - 1, squareRoot - 1)) $ zip [(i, j) | i <- [0 .. squareRoot - 1], j <- [0 .. squareRoot - 1]] (map toLower exampleBoard)
   where
     squareRoot :: Int
     squareRoot = round . sqrt . fromIntegral . length $ exampleBoard
@@ -58,10 +59,11 @@ permuteBoard b t = concatMap (permuteFrom b t S.empty T.empty) startingPositions
         c = board A.! (i, j)
         neighbours = filter (`S.notMember` visited) . filter inRange . filter (\(v1, v2) -> not (v1 == i && v2 == j)) $ [(i + x, j + y) | x <- [-1 .. 1], y <- [-1 .. 1]]
 
-        inRange ::  (Int, Int) -> Bool
-        inRange  (i', j') = let ((mini,minj),(maxi,maxj)) = bounds b
-                              in i' >= mini && i' <= maxi && j' >= minj && j' <= maxj
-        bounds :: GameBoard -> ((Int, Int),(Int, Int))
+        inRange :: (Int, Int) -> Bool
+        inRange (i', j') =
+          let ((mini, minj), (maxi, maxj)) = bounds b
+           in i' >= mini && i' <= maxi && j' >= minj && j' <= maxj
+        bounds :: GameBoard -> ((Int, Int), (Int, Int))
         bounds = A.bounds
     startingPositions = A.indices b
 
@@ -88,7 +90,7 @@ search :: T.Text -> Trie -> Bool
 search word trie = maybe False endOfWord (T.foldl' searchChar (Just trie) word)
   where
     searchChar (Just (Trie _ childs)) c = Map.lookup c childs
-    searchChar Nothing _                = Nothing
+    searchChar Nothing _ = Nothing
 
 main :: IO ()
 main = do
@@ -97,24 +99,21 @@ main = do
   dict <- TIO.readFile dictPath
   let wds = T.lines dict
   let trie = insertWords wds emptyTrie
+  putStrLn "Running boggle server at localhost:8081"
   run 8081 $ application trie
 
--- let solved = permuteBoard (gameBoard board) trie
--- let uniqueSolved = nub solved
--- let sortedSolved = sortBy (comparing (Down . T.length)) uniqueSolved
-
-
-data ResponseStruct = Resp {
-  value :: Maybe [T.Text],
-  error :: Maybe T.Text
-} deriving (Generic, ToJSON)
-
+data ResponseStruct = Resp
+  { value :: Maybe [T.Text],
+    error :: Maybe T.Text
+  }
+  deriving (Generic, ToJSON)
 
 isSquare :: Int -> Bool
-isSquare n = let intArg = fromIntegral n :: Double
-                 roundedSqrt = (round . sqrt $ intArg) :: Int
-                 s = fromIntegral roundedSqrt :: Double
-              in s == (intArg / s)
+isSquare n =
+  let intArg = fromIntegral n :: Double
+      roundedSqrt = (round . sqrt $ intArg) :: Int
+      s = fromIntegral roundedSqrt :: Double
+   in s == (intArg / s)
 
 -- A warp application that takes a 16 character string consisting of only a-z. Throws an error if the string is not 16 characters long or characters aren't correct.
 --
@@ -122,27 +121,29 @@ application :: Trie -> Application
 application trie req res = do
   let pathInfoList = pathInfo req
   if null pathInfoList
-    then res $ responseLBS status500 [(hContentType, "application/json")]
-      (encode $ Resp Nothing (Just "Empty board passed. Must be a square number (e.g. 16 chars) of characters and a-z"))
+    then
+      res $
+        responseLBS
+          status500
+          [(hContentType, "application/json")]
+          (encode $ Resp Nothing (Just "Empty board passed. Must be a square number (e.g. 16 chars) of characters and a-z"))
     else do
       let board = T.unpack $ head pathInfoList
       liftIO $ print board
       if (not . isSquare) (length board) || not (all (`elem` ['a' .. 'z']) board)
-        then res $ responseLBS status400 [(hContentType, "application/json")]
-         (encode $ Resp Nothing (Just "invalid passed. Must be a square number (e.g. 16 chars) of characters and a-z"))
+        then
+          res $
+            responseLBS
+              status400
+              [(hContentType, "application/json")]
+              (encode $ Resp Nothing (Just "invalid passed. Must be a square number (e.g. 16 chars) of characters and a-z"))
         else do
           let solved = permuteBoard (gameBoard board) trie
           let uniqueSolved = nub solved
           let sortedSolved = sortBy (comparing (Down . T.length)) uniqueSolved
           let jsonData = Resp (Just sortedSolved) Nothing
-          res $ responseLBS status200 [(hContentType, "application/json")]
-            (encode jsonData)
-
-
--- print $ search (T.pack "thta") trie
--- print $ map (`search` trie) solved
-
--- word <- TIO.getLine
--- if search word trie
---   then putStrLn "The word is in the dictionary."
---   else putStrLn "The word is not in the dictionary."
+          res $
+            responseLBS
+              status200
+              [(hContentType, "application/json")]
+              (encode jsonData)
